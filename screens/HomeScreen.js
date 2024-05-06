30860856c11ea0e73f1652f253bb89fa639f3c72
Rehannriaz <u2021438@giki.edu.pk>
2024-05-07 01:29:51 +0500

    csS

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-import { View, Text, Button, FlatList, Image } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+
 import * as ImagePicker from "expo-image-picker";
 import { Platform } from "react-native";
+
 // import * as FileSystem from 'expo-asset';
 const HomeScreen = ({ data, onNavigate }) => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageUploaded, setImageUploaded] = useState(false);
+  const [handleCalled,setHandleCalled] = useState(false);
   const pickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (status !== "granted") {
@@ -33,13 +44,13 @@ const HomeScreen = ({ data, onNavigate }) => {
       return;
     }
     setImageUploaded(false);
-    
+    setHandleCalled(true);
     let imageUri = selectedImage; // Extract URI from selectedImage
     if (Platform.OS === "android") {
       imageUri = imageUri.replace("file://", "");
     }
-    console.log("result.url = ",imageUri);
-    
+    console.log("result.url = ", imageUri);
+
     // Create a FormData object
     const formData = new FormData();
     formData.append("image", {
@@ -49,7 +60,7 @@ const HomeScreen = ({ data, onNavigate }) => {
     });
     try {
       const response = await fetch("http://10.1.155.151:6000/recognize_faces", {
-      // const response = await fetch("http://localhost:6000/recognize_faces", {
+        // const response = await fetch("http://localhost:6000/recognize_faces", {
         method: "POST",
         body: formData,
       });
@@ -57,39 +68,81 @@ const HomeScreen = ({ data, onNavigate }) => {
       if (!response.ok) {
         throw new Error("Failed to recognize faces.");
       }
-  
+
       const blob = await response.blob();
       const imageURL = URL.createObjectURL(blob);
-  
+
       // Display the image with recognized faces
       setSelectedImage(imageURL);
+      setHandleCalled(false);
     } catch (error) {
       console.error("Error recognizing faces:", error);
       setImageUploaded(true);
+      setHandleCalled(false);
       alert("Failed to recognize faces. Please try again.");
     }
   };
+  const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
   // Replace with your UI logic to display fetched data
   return (
-    <View>
-      <Text>Smart Attendance</Text>
+    <View style={{ alignItems: "center" }}>
+      <Text className="text-center text-4xl text-black">Smart Attendance</Text>
       {selectedImage && (
         <>
           <Image
             source={{ uri: selectedImage }}
-            style={{ width: 800, height: 600 }}
+            style={{
+              width: screenWidth * 0.9, // Set image width to 90% of screen width
+              height: screenHeight * 0.5, // Set image height to 50% of screen height
+              resizeMode: "contain", // Resize the image to fit within the specified dimensions
+              marginVertical: 20, // Add vertical margin for spacing
+            }}
           />
         </>
       )}
-      <Button title="Pick an Image" onPress={pickImage} />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          borderRadius: 8,
+          padding: 10,
+          alignItems: "center",
+          width: "60%", // Fix width to 60%
+          marginTop: 60, // Add margin top for spacing
+        }}
+        onPress={pickImage}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>Pick an Image</Text>
+      </TouchableOpacity>
 
       {imageUploaded && (
-        <Button
-          title="Show Attendance"
+        <TouchableOpacity
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            borderRadius: 8,
+            padding: 10,
+            alignItems: "center",
+            width: "60%", // Fix width to 60%
+            marginTop: 60, // Add margin top for spacing
+          }}
           onPress={handleNavigateToSecondScreen}
-        />
+        >
+
+          
+          <Text style={{ color: "white", fontSize: 20 }}>Show Attendance</Text>
+        </TouchableOpacity>
+
       )}
+      {handleCalled &&
+        (
+
+          <Text style={{ color: "black", fontSize: 20 }}>Recognizing Faces...</Text>
+        )
+      }
+      
+     
     </View>
   );
 };
